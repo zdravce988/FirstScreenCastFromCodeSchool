6932882dce46d829eab623380e7258ae797f5159
Aleksa <zdravce988@gmail.com>
2017-06-21 23:07:30 +0200

    Added some functionality to the page with JQeury

---
+++ app.js
@@ -0,0 +1,10 @@
+//------------------------------------
+// DOCUMENT ready
+//------------------------------------
+
+jQuery(document).ready(function() {
+  $('.js-like').on('click', function(event){
+    event.preventDefault();
+    $(this).text("Liked").closest('.news-item').addClass('is-liked');
+  });
+});
